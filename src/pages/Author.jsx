facbf2b3869e6cfd10e4a356593fcd9aff4d7ace
markwaldron7string <mark.waldron7string@gmail.com>
2026-01-30 16:18:31 -0700

    Author Section

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,61 +1,126 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
+import Skeleton from "../components/UI/Skeleton";
 
 const Author = () => {
+  const { authorId } = useParams();
+  const [authorData, setAuthorData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [followersCount, setFollowersCount] = useState(0);
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  useEffect(() => {
+    const fetchAuthorData = async () => {
+      try {
+        const response = await fetch(`https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`);
+        if (!response.ok) throw new Error(`HTTP ${response.status}`);
+        const data = await response.json();
+        setAuthorData(data);
+        setFollowersCount(data.followers);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAuthorData();
+  }, [authorId]);
+
+  const toggleFollow = () => {
+    setIsFollowing(!isFollowing);
+    setFollowersCount(isFollowing ? followersCount - 1 : followersCount + 1);
+  };
+
+  if (loading) {
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
-        <div id="top"></div>
-
-        <section
-          id="profile_banner"
-          aria-label="section"
-          className="text-light"
-          data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}
-        ></section>
-
-        <section aria-label="section">
+        <section id="profile_banner" className="text-light" style={{ background: `url(${AuthorBanner}) top` }}></section>
+        <section>
           <div className="container">
             <div className="row">
               <div className="col-md-12">
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      <Skeleton width="150px" height="150px" borderRadius="50%" /> {/* Author Image Skeleton */}
+                    </div>
+                  </div>
+                  <div className="de-flex-col" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                    <div style={{marginLeft: '15px'}}>
+                      <Skeleton width="150px" height="30px" />
+                    </div> {/* Author Name */}
+                    <div style={{marginLeft: '15px'}}>
+                      <Skeleton width="100px" height="20px" />
+                    </div> {/* Username */}
+                    <div style={{marginLeft: '15px'}}>
+                      <Skeleton width="250px" height="20px" />
+                    </div> {/* Wallet Address */}
+                  </div>
+                  <div className="profile_follow de-flex" style={{ marginLeft: 'auto', marginTop: '50px' }}>
+                    <Skeleton width="200px" height="48px" style={{ margin: '10px 0' }} /> {/* Follow Button */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
 
+
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <div id="wrapper">
+      <div className="no-bottom no-top" id="content">
+        <section
+          id="profile_banner"
+          className="text-light"
+          style={{ background: `url(${AuthorBanner}) top` }}
+        ></section>
+        <section>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="d_profile de-flex">
+                  <div className="de-flex-col">
+                    <div className="profile_avatar">
+                      <img src={authorData.authorImage || AuthorImage} alt="" />
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
+                          {authorData.authorName}
+                          <span className="profile_username">@{authorData.tag}</span>
+                          <span className="profile_wallet">{authorData.address}</span>
+                          <button id="btn_copy" title="Copy Text">Copy</button>
                         </h4>
                       </div>
                     </div>
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      <div className="profile_follower">{followersCount} followers</div>
+                      <button onClick={toggleFollow} className="btn-main">
+                        {isFollowing ? "Unfollow" : "Follow"}
+                      </button>
                     </div>
                   </div>
                 </div>
               </div>
-
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems items={authorData.items} />
                 </div>
               </div>
             </div>
